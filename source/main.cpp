382060e2273b9dd094e5ceafdf478ec116314688
Adrien Colombier <adrien.colombier@epitech.eu>
2021-03-11 11:47:13 +0100

    [MODIFY] - Move ui.cpp into lib and main test calling it

--- source/main.cpp
+++ source/main.cpp
@@ -4,9 +4,7 @@
 int main(void)
 {   
     UI ui = UI();
-    auto tag = std::string("Adedigado");
-    
-    ui.setTag(tag);
+
     std::cout << "Hello CMake hey look my UI tag : " << ui.getTag() << std::endl;
     return 0;
 }
